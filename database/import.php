3a0cef25967c70285ae64ca471dd69ccea6d41e8
abonty360 <143591727+abonty360@users.noreply.github.com>
2026-04-11 17:23:33 +0600

    Import revision-1

--- database/import.php
+++ database/import.php
@@ -1,6 +1,10 @@
 <?php
 
-$pdo = new PDO("sqlsrv:Server=mssql;Database=master", "sa", getenv('DB_PASSWORD'));
+$pdo = new PDO(
+    "sqlsrv:Server=mssql.railway.internal;Database=master",
+    "sa",
+    getenv('DB_PASSWORD')
+);
 
 $sql = file_get_contents(__DIR__ . '/init.sql');
 
